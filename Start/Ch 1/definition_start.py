864b7aedf8b9903b4810ebd60f6c23f2615702d3
Sigitas Grebliunas <sigitas.grebliunas@paragonbank.co.uk>
2026-05-20 17:35:16 +0100

    Complete definitions

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,17 @@
 
 
 # TODO: create a basic class
+class Book:
+    def __init__(self, title):
+        self.title = title
+   
 
 
 # TODO: create instances of the class
+book1 = Book('Around the World in 80 days')
+book2 = Book('War of the worlds')
 
 
 # TODO: print the class and property
+print(book1)
+print(book1.title, book1.author, book1.pages)
