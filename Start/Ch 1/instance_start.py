1f2c3ba5865d56f6f5100482d8d6580fcafc3b58
Sigitas Grebliunas <sigitas.grebliunas@paragonbank.co.uk>
2026-05-20 17:50:44 +0100

    completed instance

--- Start/Ch 1/instance_start.py
+++ Start/Ch 1/instance_start.py
@@ -5,21 +5,43 @@
 class Book:
     # the "init" function is called when the instance is
     # created and ready to be initialized
-    def __init__(self, title):
+    def __init__(self, title, author, pages, price):
         self.title = title
+        self.author = author
+        self.pages = pages
+        self.price = price
+        self.__secret = 'Test'
         # TODO: add properties
 
     # TODO: create instance methods
+    def getprice(self):
+        if hasattr(self, '_discount'):
+            return self.price * (1 - self._discount)
+        return self.price
+    
+    def setdiscount(self, discount):
+        self._discount = discount
 
 
 # TODO: create some book instances
-b1 = Book("War and Peace")
-b2 = Book("The Catcher in the Rye")
+b1 = Book("War and Peace", 'Leo Tolstoy', 1225, 29.95)
+b2 = Book("The Catcher in the Rye", 'JD Salinger', 234, 15.95)
 
 # TODO: print the price of book1
+print(b1.getprice())
 
 
 # TODO: try setting the discount
+print(b2.getprice())
+b2.setdiscount(0.25)
+print(b2.getprice())
 
 
 # TODO: properties with double underscores are hidden by the interpreter
+try:
+    print(b2.__secret) # this will throw error
+except:
+    print('Cant print __secret prop')
+  
+# __ props are prepend with class name
+print(b2._Book__secret) # this will throw error
