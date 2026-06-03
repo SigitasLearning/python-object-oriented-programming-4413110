263beae7e21c3a7bb89747575e9502555cc12a80
Sigitas Grebliunas <sigitas.grebliunas@paragonbank.co.uk>
2026-06-03 09:18:02 +0100

    completed composition

--- Start/Ch 2/composition_start.py
+++ Start/Ch 2/composition_start.py
@@ -1,25 +1,49 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using composition to build complex objects
 
+class Author:
+
+    def __init__(self, fname, lname):
+        self.fname = fname
+        self.lname = lname
+
+    def __str__(self):
+        return f'{self.fname} {self.lname}'
+
+class Chapter:
+    def __init__(self, name, pagecount):
+        self.name = name
+        self.pagecount = pagecount
+    
+    def __str__(self):
+        return f'{self.name} ({self.pagecount} pages)'
+
 
 class Book:
-    def __init__(self, title, price, authorfname, authorlname):
+    def __init__(self, title, price, author = None):
         self.title = title
         self.price = price
+        self.author = author
 
-        self.authorfname = authorfname
-        self.authorlname = authorlname
+        self.chapters = [] 
 
-        self.chapters = []
+    def add_chapter(self, chapter:Chapter):
+        self.chapters.append(chapter)
 
-    def addchapter(self, name, pages):
-        self.chapters.append((name, pages))
+    def get_page_count(self):
+        result = 0
+        for ch in self.chapters:
+            result += ch.pagecount
+        return result
 
+        
 
-b1 = Book("War and Peace", 39.0, "Leo", "Tolstoy")
+b1 = Book("War and Peace", 39.0, Author("Leo", "Tolstoy"))
 
-b1.addchapter("Chapter 1", 125)
-b1.addchapter("Chapter 2", 97)
-b1.addchapter("Chapter 3", 143)
+b1.add_chapter(Chapter("Chapter 1", 125))
+b1.add_chapter(Chapter("Chapter 2", 97))
+b1.add_chapter(Chapter("Chapter 3", 143))
 
 print(b1.title)
+print(b1.author)
+print(b1.get_page_count())
